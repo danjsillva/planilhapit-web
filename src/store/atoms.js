0e2b79e80e64b32b7136d0dab95c4c1664465718
Daniel Silva <danjsillva@gmail.com>
2020-07-04 15:54:28 -0300

    fix: corrigido estado inicial da lista de ativos

--- src/store/atoms.js
+++ src/store/atoms.js
@@ -2,11 +2,11 @@ import { atom } from "recoil";
 
 export const balanceState = atom({ key: "balanceState", default: 0 });
 
-// export const stockListState = atom({ key: "stockListState", default: [] });
+export const stockListState = atom({ key: "stockListState", default: [] });
 
-export const stockListState = atom({ key: 'stockListState', default: [
-    { symbol: 'PETR4', name: 'Petroleo Brasileiro SA Petrobras Preference Shares', grade: 2, price: 10, volume: 6 },
-    { symbol: 'SQIA3', name: 'Sinqia SA', grade: 1, price: 10, volume: 2 },
-    { symbol: 'IVVB11', name: 'ISHARESSPDGG/Ut BRL', grade: 1, price: 10, volume: 2 },
-    { symbol: 'CSNA3', name: 'Companhia Siderurgica Nacional', grade: 1, price: 10, volume: 2 },
-]})
+// export const stockListState = atom({ key: 'stockListState', default: [
+//     { symbol: 'PETR4', name: 'Petroleo Brasileiro SA Petrobras Preference Shares', grade: 2, price: 10, volume: 6 },
+//     { symbol: 'SQIA3', name: 'Sinqia SA', grade: 1, price: 10, volume: 2 },
+//     { symbol: 'IVVB11', name: 'ISHARESSPDGG/Ut BRL', grade: 1, price: 10, volume: 2 },
+//     { symbol: 'CSNA3', name: 'Companhia Siderurgica Nacional', grade: 1, price: 10, volume: 2 },
+// ]})

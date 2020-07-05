4fac6c678ac2582e68385342ada37496e2c33cb4
Daniel Silva <danjsillva@gmail.com>
2020-07-05 10:03:37 -0300

    feat: editar e remover ativos entre outras melhorias na tabela

--- src/components/StocksTable.js
+++ src/components/StocksTable.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
-import {
-  FiPlusCircle,
-  FiList,
-} from "react-icons/fi";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { FiPlusCircle, FiList, FiXCircle } from "react-icons/fi";
+import NumberFormat from "react-number-format";
 import classNames from "classnames";
 
+import { stockListState } from "../store/atoms";
 import { stockListFullState } from "../store/selectors";
 
 const EmptyState = () => {
@@ -39,80 +38,152 @@ const EmptyState = () => {
   );
 };
 
-const Table = ({ stocks }) => {
+const StocksTable = () => {
+  const [stocks, setStocks] = useRecoilState(stockListState);
+  const stocksFull = useRecoilValue(stockListFullState);
+
+  const handleBlurCell = (symbol, field, value) => {
+    setStocks(
+      stocks.map((stock) =>
+        stock.symbol === symbol ? { ...stock, [field]: value } : stock
+      )
+    );
+  };
+
+  const handleClickRemove = (symbol) => {
+    setStocks(stocks.filter((stock) => stock.symbol !== symbol));
+  };
+
   return (
     <>
-      <div className="card card-body">
-        <div className="row font-weight-bold">
-          <div className="col-2">Ativo</div>
-          <div className="col-1 text-right">Nota</div>
-          <div className="col-1 text-right">Preço</div>
-          <div className="col-2 text-right">Quantidade atual</div>
-          <div className="col-2 text-right">Total atual</div>
-          <div className="col-2 text-right">Quantidade ideal</div>
-          <div className="col-2 text-right">Total ideal</div>
-        </div>
-      </div>
-
-      {stocks.map((stock) => (
-        <div key={stock.symbol} className="card card-body pointer mt-1">
-          <div className="row">
-            <div className="col-2">
-              <b>{stock.symbol}</b>
-              <small className="d-block text-truncate">{stock.name}</small>
-            </div>
-            <div className="col-1 text-right">{stock.grade}</div>
-            <div className="col-1 text-right">R$ {stock.price.toFixed(2)}</div>
-            <div className="col-2 text-right">{stock.volume}</div>
-            <div className="col-2 text-right">
-              <b>R$ {stock.total.toFixed(2)}</b>
-              <small className="d-block text-muted">
-                {stock.percent.toFixed(1)}%
-              </small>
-            </div>
-            <div className="col-2 text-right">
-              {stock.idealVolume}
-              <small
-                className={classNames([
-                  "d-block",
-                  {
-                    "text-success": stock.differenceVolume > 0,
-                    "text-danger": stock.differenceVolume < 0,
-                  },
-                ])}
-              >
-                ({stock.differenceVolume})
-              </small>
-            </div>
-            <div className="col-2 text-right">
-              <b>R$ {stock.idealTotal.toFixed(2)}</b>
-              <small className="d-block text-muted">
-                {stock.idealPercent.toFixed(1)}%
-              </small>
+      {stocksFull.length ? (
+        <>
+          <div className="card card-body">
+            <div className="row align-items-end font-weight-bold">
+              <div className="col-3">Ativo</div>
+              <div className="col-1 text-right">Nota</div>
+              <div className="col-1 text-right">Preço</div>
+              <div className="col-1 text-right">Quant atual</div>
+              <div className="col-2 text-right">Total atual</div>
+              <div className="col-1 text-right">Quant ideal</div>
+              <div className="col-2 text-right">Total ideal</div>
+              <div className="col-1 text-right"></div>
             </div>
           </div>
-        </div>
-      ))}
 
-      <div
-        className="card card-body pointer mt-1"
-        data-toggle="modal"
-        data-target="#stockModal"
-      >
-        <div className="row">
-          <div className="col text-center">
-            <FiPlusCircle />
+          {stocksFull.map((stock) => (
+            <div key={stock.symbol} className="card card-body mt-1">
+              <div className="row align-items-center">
+                <div className="col-3">
+                  <b>{stock.symbol}</b>
+                  <small className="d-block text-truncate">{stock.name}</small>
+                </div>
+                <div className="col-1 text-right">
+                  <NumberFormat
+                    defaultValue={0}
+                    value={stock.grade}
+                    prefix=""
+                    thousandSeparator="."
+                    decimalSeparator=","
+                    decimalScale={0}
+                    fixedDecimalScale={false}
+                    allowLeadingZeros={false}
+                    onValueChange={(values) =>
+                      handleBlurCell(stock.symbol, "grade", values.floatValue)
+                    }
+                    className="form-control"
+                    autoFocus
+                  />
+                </div>
+                <div className="col-1 text-right">
+                  <NumberFormat
+                    defaultValue={0}
+                    value={stock.price}
+                    prefix=""
+                    thousandSeparator="."
+                    decimalSeparator=","
+                    decimalScale={2}
+                    fixedDecimalScale={true}
+                    allowLeadingZeros={false}
+                    onValueChange={(values) =>
+                      handleBlurCell(stock.symbol, "price", values.floatValue)
+                    }
+                    className="form-control"
+                    autoFocus
+                  />
+                </div>
+                <div className="col-1 text-right">
+                  <NumberFormat
+                    defaultValue={0}
+                    value={stock.volume}
+                    prefix=""
+                    thousandSeparator="."
+                    decimalSeparator=","
+                    decimalScale={0}
+                    fixedDecimalScale={false}
+                    allowLeadingZeros={false}
+                    onValueChange={(values) =>
+                      handleBlurCell(stock.symbol, "volume", values.floatValue)
+                    }
+                    className="form-control"
+                    autoFocus
+                  />
+                </div>
+                <div className="col-2 text-right">
+                  <b>R$ {stock.total.toFixed(2)}</b>
+                  <small className="d-block text-muted">
+                    {stock.percent.toFixed(1)}%
+                  </small>
+                </div>
+                <div className="col-1 text-right">
+                  {stock.idealVolume}
+                  <small
+                    className={classNames([
+                      "d-block",
+                      {
+                        "text-success": stock.differenceVolume > 0,
+                        "text-danger": stock.differenceVolume < 0,
+                      },
+                    ])}
+                  >
+                    ({stock.differenceVolume})
+                  </small>
+                </div>
+                <div className="col-2 text-right">
+                  <b>R$ {stock.idealTotal.toFixed(2)}</b>
+                  <small className="d-block text-muted">
+                    {stock.idealPercent.toFixed(1)}%
+                  </small>
+                </div>
+                <div className="col-1 text-right">
+                  <button
+                    className="btn btn-outline-white"
+                    onClick={(e) => handleClickRemove(stock.symbol)}
+                  >
+                    <FiXCircle />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <div
+            className="card card-body pointer mt-1"
+            data-toggle="modal"
+            data-target="#stockModal"
+          >
+            <div className="row">
+              <div className="col text-center">
+                <FiPlusCircle />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <EmptyState />
+      )}
     </>
   );
-};
-
-const StocksTable = () => {
-  const stocks = useRecoilValue(stockListFullState);
-
-  return <>{stocks.length ? <Table stocks={stocks} /> : <EmptyState />}</>;
 };
 
 export default StocksTable;
